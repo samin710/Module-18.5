fcbdc65b8d92d66f1010c856585906977f7e8f46
samin710 <smsaminii112@gmail.com>
2025-02-08 18:46:14 +0600

    add remove done

---
+++ addRemove.js
@@ -0,0 +1,14 @@
+// Declare an array of 3 tourist destinations
+// Add a new tourist destination to your tourist array
+// Add two more to your array
+// Remove the last tourist destination you have added
+// display the final array as output
+
+let tArray = ['Switzerland', 'France', 'Denmark'];
+console.log(tArray);
+tArray.push('Iceland');
+console.log(tArray);
+tArray.push('Netherlands', 'Japan');
+console.log(tArray);
+tArray.pop();
+console.log(tArray);
