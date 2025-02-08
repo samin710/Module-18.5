44d5d9e0c2006f231e5e04e24de553d1e24c3fbb
samin710 <smsaminii112@gmail.com>
2025-02-08 18:51:54 +0600

    includes done

---
+++ includes.js
@@ -0,0 +1,7 @@
+// Create an array of books containing different book.
+// Use the includes method to check if the array contains a javascript book.
+// Print a message to the console indicating whether the element is present in the array or not.
+
+let books = ['first', 'second', 'third', 'fourth', 'fifth'];
+
+console.log(books.includes("javascript"));
