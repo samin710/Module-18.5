25ddeaff8c561c9e9db8bdfac6204d14c1b3f7ad
samin710 <smsaminii112@gmail.com>
2025-02-08 18:39:16 +0600

    declare done

---
+++ declareArray.js
@@ -0,0 +1,13 @@
+// Declare an array with 5 elements containing fruits
+// console log the 3rd index element
+// change the value of the 2nd index element to jambura
+// console log the final array
+
+let fruitsArray = ['apple', 'pine-apple', 'orange', 'water-melon', 'banana'];
+console.log("Third index: "+ fruitsArray[3]);
+console.log(fruitsArray);
+
+fruitsArray[2] = 'jambura';
+console.log(fruitsArray);
+
+
