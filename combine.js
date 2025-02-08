872477d76c1887eea9d981626d8ce9b1d31fe55b
samin710 <smsaminii112@gmail.com>
2025-02-08 19:02:47 +0600

    combine done

---
+++ combine.js
@@ -0,0 +1,12 @@
+// Create two arrays of your choice.
+// Use the concat method to combine the two arrays into a new array.
+// Print both the original arrays and the combined array using console.log().
+
+let books = ["first", "second", "third", "fourth", "fifth"], tArray = ["Switzerland", "France", "Denmark"];
+
+let combineArray = books.concat(tArray);
+console.log(books);
+console.log(tArray);
+
+console.log(combineArray);
+
